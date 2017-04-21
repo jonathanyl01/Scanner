2ea7cbb84a5835e6389288ac1d664be6609f2766
jonathanyl01 <17768373003@163.com>
2017-04-21 14:31:30 +0800

    二维码扫描的实现

---
+++ ListViewDemo/app/src/main/java/com/example/administrator/listviewdemo/MyAdapter.java
@@ -0,0 +1,7 @@
+package com.example.administrator.listviewdemo;
+
+/**
+ * Created by Administrator on 2017/4/14 0014.
+ */
+public class MyAdapter {
+}
---
+++ Scan/app/src/com/example/administrator/scan/apshare/ShareEntryActivity.java
@@ -0,0 +1,7 @@
+package com.example.administrator.scan.apshare;
+
+import cn.sharesdk.alipay.utils.AlipayHandlerActivity;
+
+public class ShareEntryActivity extends AlipayHandlerActivity {
+
+}
